21e23095e96f081ef9195ad931f397ea4b295380
André Posman <andre.posman@gmail.com>
2020-07-15 00:00:52 -0300

    small improvents on project architecture

--- improvements.md
+++ improvements.md
@@ -1,10 +1,11 @@
-- seguir o padrão JS de pastas, arquivos e funções com letras minusculas
+- seguir o padrão JS de pastas, arquivos e funções com letras minusculas (X)
 - ajustar estrutura de teste. colocar o arquivo de teste na pasta do arquivo normal, e extrair para mais arquivos diferentes (dar uma olhada no jest)
 - se nao for ter outros dominios, remover a pasta de movie tendo em vista que o projeto só tem um dominio
-- remover alguns const, tendo em vista que as vezes não é usado (ex: const movieSearched = await MovieService ... => return await MovieService...)
-- ja existe o try catch no Controller, nao existe necessidade de ter no service (readability, boas praticas pegar em lugar unico)
+- remover alguns const, tendo em vista que as vezes não é usado (ex: const movieSearched = await MovieService ... => return await MovieService...) (X)
+- ja existe o try catch no Controller, nao existe necessidade de ter no service (readability, boas praticas pegar em lugar unico) (X)
 - ver sobre abrir/fechar conexão com mongo, pode onerar.
-- controller deveria ter a responsabilidade de trabalhar com req, res. 
+- controller deveria ter a responsabilidade de trabalhar com req, res.
 - rever rota do healthcheck, usar framework do express e eliminar rota (olhar doc)
 - rever uso do validatorJs
 - documentar
+- jwt (injeção na rota)
--- src/movie/controller/movieController.js
+++ src/movie/controller/movieController.js
@@ -1,63 +1,58 @@
 const MovieService = require("../services/movieService");
 const SuccessModel = require('../../helpers/models/successModel');
 const ErrorModel = require('../../helpers/models/errorModel');
-const { search } = require("../../routes");
 
+exports.create = async (req, res) => {
+  try {
+    await MovieService.create(req);
 
-module.exports = {
-  async create(req, res) {
-    try {
-      await MovieService.create(req);
-  
-      return res.status(SuccessModel.CreatedWithSuccess.statusCode).send({
-        success: true,
-        message: SuccessModel.CreatedWithSuccess.data
-      })
-    } catch (err) {
-      return res.status(ErrorModel.CreationFailed.statusCode).send({
-        success: false,
-        message: ErrorModel.CreationFailed.data
-      })
-    }
-  },
-
-  async findAll(req, res) {
-    try {
-      const allMovies = await MovieService.findAll(req, res);
-
-      return allMovies;
-    } catch (err) {
-      return res.status(ErrorModel.MovieNotFound.statusCode).send({
-        success: false,
-        message: ErrorModel.MovieNotFound.data
-      })
-    }
-  },
-
-  async search(req,res){
-    try {
-      return await MovieService.search(req, res)
-
-    } catch (error) {
-      return res.status(ErrorModel.MovieNotFound.statusCode).send({
-        success: false,
-        message: ErrorModel.MovieNotFound.data
-      })
-    }
-  },
-
-  async delete(req,res){
-    try {
-      await MovieService.delete(req, res)
-      
-      res.send(SuccessModel.DeletedWithSuccess)
-
-    } catch (error) {
-      return res.status(ErrorModel.MovieNotFound.statusCode).send({
-        success: false,
-        message: ErrorModel.MovieNotFound.data
-      })
-    }
+    return res.status(SuccessModel.CreatedWithSuccess.statusCode).send({
+      success: true,
+      message: SuccessModel.CreatedWithSuccess.data
+    })
+  } catch (err) {
+    return res.status(ErrorModel.CreationFailed.statusCode).send({
+      success: false,
+      message: ErrorModel.CreationFailed.data
+    })
   }
+}
 
-};
+exports.findAll = async (req, res) => {
+  try {
+    const allMovies = await MovieService.findAll(req, res);
+
+    return allMovies;
+  } catch (err) {
+    return res.status(ErrorModel.MovieNotFound.statusCode).send({
+      success: false,
+      message: ErrorModel.MovieNotFound.data
+    })
+  }
+}
+
+exports.search = async (req, res) => {
+  try {
+    return await MovieService.search(req, res)
+
+  } catch (error) {
+    return res.status(ErrorModel.MovieNotFound.statusCode).send({
+      success: false,
+      message: ErrorModel.MovieNotFound.data
+    })
+  }
+}
+
+exports.delete = async (req, res) => {
+  try {
+    await MovieService.delete(req, res)
+
+    res.send(SuccessModel.DeletedWithSuccess)
+
+  } catch (error) {
+    return res.status(ErrorModel.MovieNotFound.statusCode).send({
+      success: false,
+      message: ErrorModel.MovieNotFound.data
+    })
+  }
+}
